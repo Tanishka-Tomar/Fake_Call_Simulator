d81b5ce55a0cdbb518cb80aaf17b9e7396aa8ee3
Tanishka <tanishkatomar46@gmail.com>
2026-09-05 21:42:41 +0530

    name change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Fake_Call_Simulator/',
+  base: '/False_Call/',
   plugins: [
     react(),
     tailwindcss(),
